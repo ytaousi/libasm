14527724df781e3a577c06aa0d03ae1f0d111b50
ytaousi <46757421+ytaousi@users.noreply.github.com>
2021-04-05 13:24:05 +0200

    Update ft_write.s

--- ft_write.s
+++ ft_write.s
@@ -12,9 +12,17 @@
 
 			section	.text
 			global	_ft_write
+			extern  ___error
 ; DELETE all unpreserved registers by _write
 
 _ft_write:								; fd = rdi, buffer = rsi, bytes = rdx
 			mov		rax, 0x2000004
 			syscall
+			jc error
+			ret
+
+error:			push rax		;save errno
+			call ___error		;point rax to the external variable errno
+			pop  qword[rax]
+			mov  rax,-1
 			ret
