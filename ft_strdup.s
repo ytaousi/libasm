ea612c9e3a5642633ef3e90652aa14481511f3db
ytaousi <46757421+ytaousi@users.noreply.github.com>
2021-04-19 10:07:27 +0000

    Update ft_strdup.s

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,38 +1,35 @@
-			global	_ft_strdup
-			extern	_malloc
+section     .text
+global      _ft_strdup
 
-_ft_strdup:									; rdi = src
-			cmp		rdi, 0
-			jz		error					; src is NULL
-len_start:
-			xor		rcx, rcx				; i = 0
-			jmp		len_compare
-len_increment:
-			inc		rcx						; i++
-len_compare:
-			cmp		BYTE [rdi + rcx], 0		; str[i] == 0
-			jne		len_increment
-malloc_start:
-			inc		rcx						; length++
-			push	rdi						; save src
-			mov		rdi, rcx
-			call	_malloc					; rax = _malloc(length)
-			pop		rdi						; restore src
-			cmp		rax, 0
-			jz		error					; malloc return NULL
-copy_start:
-			xor		rcx, rcx				; i = 0
-			xor		rdx, rdx				; tmp = 0
-			jmp		copy_copy
-copy_increment:
-			inc		rcx
-copy_copy:
-			mov		dl, BYTE [rdi + rcx]
-			mov		BYTE [rax + rcx], dl
-			cmp		dl, 0
-			jnz		copy_increment
-			jmp		return
-error:
-			xor		rax, rax
+extern      _malloc
+extern      _ft_strlen
+extern      _ft_strcpy
+extern      ___error
+
+_ft_strdup:
+    xor     rax, rax
+    xor     rcx, rcx
+    xor     r9, r9
+
+    push    rdi
+    call    _ft_strlen
+    mov     rdi, rax
+    call    _malloc
+    jc      error
+    cmp     rax, 0
+    je      return
+    mov     rdi, rax
+    pop     rsi
+    call    _ft_strcpy
+    ret
+
 return:
-			ret
+    call    ___error
+    mov     rax, 0
+	ret
+
+error:
+    push    rax
+    mov     BYTE [rax], 12
+    mov     rax, -1
+    ret
