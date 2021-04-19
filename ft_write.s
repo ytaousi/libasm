a004eda080a466d5a3467538ba36303e25e769bf
ytaousi <46757421+ytaousi@users.noreply.github.com>
2021-04-19 02:32:42 +0000

    Update ft_write.s

--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,7 @@
 			section	.text
 			global	_ft_write
 			extern  ___error
-; DELETE all unpreserved registers by _write
+
 
 _ft_write:								; fd = rdi, buffer = rsi, bytes = rdx
 			mov		rax, 0x2000004
