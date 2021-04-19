848b40d5071a52c0e844ff24baff89f75f6ff7b2
ytaousi <46757421+ytaousi@users.noreply.github.com>
2021-04-19 02:25:22 +0000

    Update ft_strcpy.s

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -6,11 +6,14 @@ _ft_strcpy:									; dst = rdi, src = rsi
 			mov		r10, rdi				; rcx = counter = 0
 			mov		r11, rsi				
 			mov		rcx, 0					
-increment:
-			inc		rcx
 copy:
-			cmp		dl, 0
-			jnz		increment
+			cmp     BYTE [r11 + rcx] , 0
+    			je      return
+    			mov     r9, [r11 + rcx]
+    			mov     [r10 + rcx], r9
+    			inc     rcx
+    			call    copy
 return:
-			mov		rax, rdi				; return dst
+			mov	BYTE [r10 + rcx], 0				; return dst
+			mov	rax, r10
 			ret
